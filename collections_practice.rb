9dfe7f881272bde2258b2644cef8b1491db115ca
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-06 02:44:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array[1],array[2]=array[2],array[1]
+  array
+end
+
+def swap_elements_from_to(array,index,destination_index)
+  array[index]<=>array[destination_index]
+  array
+end
